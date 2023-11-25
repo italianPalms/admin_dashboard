134950d0bfa597c4575f73eccebefcabb03214fa
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-11-25 16:27:02 +0100

    add theme for dark and light mode, add icons, dark and ligt mode to the topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,6 +1,23 @@
+import { Box, IconButton, useTheme } from '@mui/material';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material';
+import { LightModeOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { DarkModeOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { NotificationsOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { SettingsOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { PersonOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { SearchIcon } from '@mui/icons-material/LightModeOutlined';
 
 const Topbar = () => {
-    return <div>Topbar</div>
-}
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
+    return <Box display="flex" justifyContent="space-between" p={2}>
+
+        <IconButton sx={{ display: "flex" }}></IconButton>
+    </Box>
+};
 
 export default Topbar;
--- src/theme.js
+++ src/theme.js
@@ -147,7 +147,7 @@ export const themeSettings = (mode) => {
                     }
                 } : {
                     primary: {
-                        main: colors.primary[500], 
+                        main: colors.primary[100], 
                     }, 
                     secondary: {
                         main: colors.greenAccent[500], 
@@ -158,10 +158,61 @@ export const themeSettings = (mode) => {
                         light: colors.grey[100], 
                     }, 
                     background: {
-                        default: colors.primary[500], 
-                    }
+                        default: "#fcfcfc", 
+                    },
                 }
             )
+        }, 
+        typography: {
+            fontFamily: ["Roboto", "sans-serif"].join(","),
+            fontSize: 12, 
+            
+            h1: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 40, 
+            },
+            h2: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 32, 
+            },
+            h3: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 24, 
+            },
+            h4: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 20, 
+            },
+            h5: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 16, 
+            },
+            h6: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 14, 
+            }
         }
-    }
+    };
+};
+
+// contect for the color mode
+
+export const ColorModeContext = createContext({
+    toggleColorMode: () => {}
+});
+
+export const useMode = () => {
+    const [mode, setMode] = useState('dark');
+
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () => 
+            setMode((prev) => (prev === 'light' ? 'dark' : 'light')),
+        }),
+        [] 
+    );
+
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+    return [theme, colorMode];
 }
